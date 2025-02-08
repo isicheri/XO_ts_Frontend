e7599bff60efc53aeddd5c1c02cfc131217b0a5a
Isicheri <tech2dom@gmail.com>
2025-02-08 10:24:22 +0100

    working on the frontend to production

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,107 @@
+let currentPlayer: string;
+let pminElement = document.querySelector('.time-min') as HTMLParagraphElement;
+let psecElement = document.querySelector(".time-sec") as HTMLParagraphElement;
 
 
-export function makeMove(row:number,col:number) {}
+//ill use this when im connected to the backend
+// export function generateBoardCell(boardElement: HTMLDivElement) {
+//  for (let row = 0; row < 3; row++) {
+//    for (let col = 0; col < 3; col++) {
+//      const cell = document.createElement("div")
+//      cell.classList.add("cell")
+//     cell.addEventListener("click",() => makeMove(row,col))
+//     boardElement.appendChild(cell);
+//    }
+//  }
+// }
 
-export function updateUi() {}
+ function startGame(sec:number,min: number) {
+    let timer = setInterval(() => {
+        pminElement.textContent = min < 10 ? `0${min}` : `${min}`;
+        psecElement.textContent = sec < 10 ? `0${sec}` : `${sec}`;
+        sec -= 1;
+        if (sec == 0) {
+            min -= 1;
+            sec = 59;
+        }
+        if (min == 0 && sec == 0) {
+            clearInterval(timer);  
+            console.log("Game over, no winner");
+        }
+    }, 1000);
+
+}
+
+export function generateBoardCell(boardElement:HTMLDivElement) {
+    startGame(59,1)
+     for (let row = 0; row < 3; row++) {
+   for (let col = 0; col < 3; col++) {
+     const cell = document.createElement("div")
+     cell.classList.add("cell")
+     cell.setAttribute("data-row", row.toString());
+      cell.setAttribute("data-col", col.toString());
+    cell.addEventListener("click",() => checkMove(row,col,cell))
+    boardElement.appendChild(cell);
+   }
+ }
+}
+
+let board:(string | null)[][] =  [
+    [null,null,null],
+    [null,null,null],
+    [null,null,null],
+]
+
+
+function checkMove(row: number,col:number,cell:HTMLDivElement):void {
+if(board[row][col]) return;
+currentPlayer = currentPlayer === "X" ? "O" : "X"
+if(board[row][col] === null) {
+    board[row][col] = currentPlayer;
+    cell.textContent = currentPlayer;
+    if(checkWinner(row,col)) {
+        console.log(`${currentPlayer} wins!`);
+        resetBoard();
+        return;
+    }
+}
+let empty_filled = board.flat().every((element) => element !== "" && element !== null);
+if(empty_filled) {
+  console.log("it is a draw")  
+  resetBoard();
+  return
+}
+}
+
+
+function resetBoard() {
+    currentPlayer = "X";
+    setTimeout(() => {
+        board =  [
+            [null,null,null],
+            [null,null,null],
+            [null,null,null],
+        ]
+        document.querySelectorAll(".cell").forEach((cell) =>  {
+          cell.textContent = "";
+        })
+    }, 2000)
+}
+
+
+function checkWinner(row:number,col:number) {
+ if(board[row][0] === board[row][1] && board[row][1] === board[row][2]) {
+    return true;
+ }
+ if(board[0][col] === board[1][col] && board[1][col] === board[2][col]) {
+    return true;
+ }
+  // Check the diagonals
+  if (row === col && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+    return true;
+  }
+  if (row + col === 2 && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+    return true;
+  } 
+  return false;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
-import "./style.css"
+import "./css/style.css"
 import "animate.css"
-import {io,Socket} from "socket.io-client";
-
-// const socket:Socket  = io("http://localhost:8001/");
-
-
+import { generateBoardCell } from "./helpers";
 
 const btnCreateRoom = document.querySelector(".btn-create-room") as HTMLButtonElement;
 const btnJoinRoom = document.querySelector(".btn-join-room") as HTMLButtonElement;
 const descText = document.querySelector(".desc-text") as HTMLParagraphElement;
 const closeXbtn = document.querySelector(".x") as HTMLDivElement;
+const board = document.querySelector(".board") as HTMLDivElement;
+generateBoardCell(board);
 let text = ` Hey there,this is just a greeting from the creator AKA Madara,
         this is an online platform where visitor(anybody on the site currently) create or join a room to play or participate as a spectator in a Tic-Tac-Toe game ,adding comments or suggest which would be coming in later features,other features may include betting with users or participants,but for now lets first see if the site might be something in the later future,please do well to enjoy the game,
         Best regards from the creator,@Madara.`;
@@ -18,7 +16,11 @@ let text = ` Hey there,this is just a greeting from the creator AKA Madara,
             (document.querySelector(".create-room-form-con") as HTMLDivElement).style.display ="flex";
             (document.querySelector(".join-room-form-con") as HTMLDivElement).style.display = "none";
         })
-        
+document.querySelectorAll("#form").forEach((form) => {
+form.addEventListener("submit",(e) => {
+    e.preventDefault()
+})
+}) 
 closeXbtn.addEventListener("click",() => {
     (document.querySelector(".main-con-room-form") as HTMLDivElement).style.display = "none";
 })
@@ -39,9 +41,13 @@ btnJoinRoom?.addEventListener("click",() => {
     (document.querySelector(".main-con-room-form") as HTMLDivElement).style.display = "flex";
     (document.querySelector(".create-room-form-con") as HTMLDivElement).style.display ="none";
     (document.querySelector(".join-room-form-con") as HTMLDivElement).style.display = "flex";
+    (document.querySelector("#join-change-path") as HTMLButtonElement).addEventListener("click",() => {
+        window.location.pathname = "/room";
+    })
 });
 
 window.onload = () => {
     (document.querySelector(".main-con-room-form") as HTMLDivElement).style.display = "none";
     animateWordSlice(text);
 }
+// window.addEventListener("beforeunload",(e) => {})
